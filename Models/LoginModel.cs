941c3d99b31f286b7b88b4b05844450845625f30
Ba1nky <95933124+Ba1nky@users.noreply.github.com>
2022-11-09 22:19:09 +0200

    Add files via upload

---
+++ Models/LoginModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ComputerStore.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        public string UserName { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+    }
+}
